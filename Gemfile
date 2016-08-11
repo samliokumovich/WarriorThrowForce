30f71144ac3b3b9e56bb1cca3c3cd6e94b674389
Samuel Liokumovich <191liokumovichs@student.dist113.org>
2016-08-10 22:48:15 -0500

    added secure password to users

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
+#has_secure_password
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+
   validates :username, presence:true,
   uniqueness: {case_sensitive: false},
    length: { minimum: 3, maximum: 25 }
@@ -10,4 +12,5 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
    length: { maximum: 105 },
    format: { with: VALID_EMAIL_REGEX }
 
+   has_secure_password
 end
